76c4ef5311d68ea0a64814db7ecbbbf8601722ae
isrameve <israelmedive@gmail.com>
2025-05-22 15:39:54 -0500

    First commit: basic structure of the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MyComponent from "./components/Button";
+function App() {
+  return (
+    <div>
+      <h1>Mi Aplicación React</h1>
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+function MyComponent() {
+  return (
+    <div>
+      <p>Este es mi primer componente</p>
+    </div>
+  );
+}
+
+export default MyComponent;
