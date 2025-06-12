d5d07c34527c631c2bbad9582ad2b20dbe13cc6f
isrameve <israelmedive@gmail.com>
2025-06-12 17:37:59 -0500

    lista la lógica para almacenar el nuevo usuario y sus preferencias en el estado local user del componente UserForm

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import FullCenterSection from "../../pages/FullCenterSection";
 import CardBackgroundGrey from "../CardBackgroundGrey/CardBackgroundGrey";
 import Button from "../Button/Button";
@@ -5,6 +7,36 @@ import Button from "../Button/Button";
 import styles from "./UserForm.module.scss";
 
 const UserForm = () => {
+  const [inputName, setInputName] = useState("");
+  const [inputAge, setInputAge] = useState("");
+  const [inputTimeWork, setInputTimeWork] = useState("");
+  const [inputTimeRest, setInputTimeRest] = useState("");
+  const [user, setUser] = useState({
+    name: "",
+    age: undefined,
+    timeWork: undefined,
+    timeRest: undefined,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setUser({
+      name: inputName,
+      age: inputAge,
+      timeWork: inputTimeWork,
+      timeRest: inputTimeRest,
+    });
+
+    setInputName("");
+    setInputAge("");
+    setInputTimeWork("");
+    setInputTimeRest("");
+
+    alert(`Usuario guardado: ${inputName}`);
+  };
+  console.log(user);
+
   return (
     <FullCenterSection position={"fullHeightBottom"}>
       <h2 className={styles.primaryText}>Hagámos una rápida configuración</h2>
@@ -14,25 +46,69 @@ const UserForm = () => {
             Con estos datos tendrémos la información necesaria para usar la app
           </h3>
         </div>
-        <form action="">
+        <form onSubmit={handleSubmit} action="#">
           <div className={styles.ContainerForm}>
             <div className={styles.formGroup}>
-              <input type="text" placeholder="Tu nombre:" />
+              <input
+                onChange={(e) => setInputName(e.target.value)}
+                type="text"
+                value={inputName}
+                placeholder="Tu nombre:"
+              />
             </div>
             <div className={styles.formGroup}>
-              <input type="text" placeholder="Tu edad:" />
+              <input
+                onChange={(e) => setInputAge(e.target.value)}
+                type="number"
+                value={inputAge}
+                placeholder="Tu edad:"
+              />
             </div>
             <div className={styles.formGroup}>
               <label className={styles.formLabel} htmlFor="">
                 ¿De cuanto será tu tiempo de trabajo?
               </label>
-              <input type="number" />
+              <select
+                onChange={(e) => setInputTimeWork(e.target.value)}
+                className={styles.formLabel}
+                name=""
+                id=""
+              >
+                <option value="undefined">
+                  Selecciona tu tiempo de trabajo
+                </option>
+                <option value="30">30 minutos</option>
+                <option value="45">45 munitos</option>
+                <option value="60">1 hora</option>
+              </select>
+              {/* <input
+                
+                type="number"
+                value={inputTimeWork}
+              /> */}
             </div>
             <div className={styles.formGroup}>
               <label className={styles.formLabel} htmlFor="">
                 ¿Cuanto durará tu pausa activa?
               </label>
-              <input type="number" />
+
+              <select
+                onChange={(e) => setInputTimeRest(e.target.value)}
+                className={styles.formLabel}
+                name=""
+                id=""
+              >
+                <option value="undefined">
+                  Selecciona tu tiempo de pausa activa
+                </option>
+                <option value="5">5 munitos</option>
+                <option value="15">15 minutos</option>
+                <option value="30">30 minutos</option>
+              </select>
+              {/* <input
+                type="number"
+                value={inputTimeRest}
+              /> */}
             </div>
           </div>
 
