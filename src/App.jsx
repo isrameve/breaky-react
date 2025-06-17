456b77a062905245a3b430e9e39473a460e30d72
isrameve <israelmedive@gmail.com>
2025-06-17 12:05:00 -0500

    Agregado el componente BreakTimer para el paso del tiempo durante la pausa activa

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import FullCenterSection from "./pages/FullCenterSection";
 import UserForm from "./components/UserForm/UserForm";
 import WorkigTimer from "./components/WorkigTimer/WorkigTimer";
+import BreakTimer from "./components/BreakTimer/BreakTimer";
 
 import useLocalStorage from "./hooks/useLocalStorage";
 
@@ -14,6 +15,10 @@ function App() {
       <UserForm />
       <FullCenterSection>
         <WorkigTimer timeWorking={userProfile.workTime} />
+      </FullCenterSection>
+      <FullCenterSection>
+        {/* Pronto tendrán que compartir el contenedor FullCenterSection pero por ahora para poder testear lo hacemos así para que cada secciópn esté separada. Mas adelante ocultarémos cada Card(Timer) según sea necesario */}
+        <BreakTimer breakTime={userProfile.breakTime} />
       </FullCenterSection>
     </div>
   );
--- src/components/BreakTimer/BreakTimer.jsx
+++ src/components/BreakTimer/BreakTimer.jsx
@@ -4,22 +4,22 @@ import useTimer from "../../hooks/useTimer";
 import CardBackgroundGrey from "../CardBackgroundGrey/CardBackgroundGrey";
 import Button from "../Button/Button";
 
-import styles from "./WorkingTimer.module.scss";
+import styles from "./BreakTimer.module.scss";
 
-const WorkigTimer = ({ timeWorking }) => {
+const BreakTimer = ({ breakTime }) => {
   const { time, formattedTime, isRunning, start, pause, reset } =
-    useTimer(timeWorking);
+    useTimer(breakTime);
 
   useEffect(() => {
     if (time === 0) {
-      alert("Ahora es tiempo de una pausa activa");
+      alert("A trabajar!!");
     }
   }, [time]);
 
   return (
     <CardBackgroundGrey>
       <h3 className={styles.primaryText}>{formattedTime}</h3>
-      <h4 className={styles.secondaryText}>Hasta la pausa activa</h4>
+      <h4 className={styles.secondaryText}>De pausa activa</h4>
       <div className={styles.buttonsContainer}>
         <Button
           onClick={!isRunning ? start : pause}
@@ -27,7 +27,7 @@ const WorkigTimer = ({ timeWorking }) => {
         />
         <Button
           typeOfButton="secondaryButton"
-          onClick={() => reset(timeWorking)}
+          onClick={() => reset(breakTime)}
           label={"Reiniciar"}
         />
       </div>
@@ -35,4 +35,4 @@ const WorkigTimer = ({ timeWorking }) => {
   );
 };
 
-export default WorkigTimer;
+export default BreakTimer;
