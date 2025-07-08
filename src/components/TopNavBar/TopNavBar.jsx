45b83590404bc2c4d725c144164e0f4e1b00eeff
isrameve <israelmedive@gmail.com>
2025-07-07 20:20:04 -0500

    implementada la lógica para que se muestre el nombre del user en la NavBar

--- src/components/TopNavBar/TopNavBar.jsx
+++ src/components/TopNavBar/TopNavBar.jsx
@@ -1,13 +1,22 @@
-// import useLocalStorage from "../../hooks/useLocalStorage";
+import useLocalStorage from "../../hooks/useLocalStorage";
 
 import styles from "./TopNavBar.module.scss";
 
 const TopNavBar = ({ children }) => {
+  const initialUserConfig = {
+    name: "",
+    age: undefined,
+    workTime: undefined,
+    breakTime: undefined,
+  };
+
+  const [userProfile] = useLocalStorage("userProfile", initialUserConfig);
+
   return (
     <div className={styles.navBar}>
       <div className={styles.logoAndUserId}>
-        <img src="" alt="logo-breaky" />
-        <h3>User</h3>
+        <img src="any" alt="logo-breaky" />
+        <h3>Hola {userProfile.name || ""}!!</h3>
       </div>
       <div>{children}</div>
     </div>
